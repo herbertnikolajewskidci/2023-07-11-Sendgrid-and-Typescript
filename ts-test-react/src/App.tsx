3fc56661436002bdc5dbfa7ae67ade89ae8b24b6
Herbert Nikolajewski <herbert.nikolajewksi@digitalcareerinstitute.org>
2023-07-11 08:38:57 +0200

    final final

--- ts-test-react/src/App.tsx
+++ ts-test-react/src/App.tsx
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+interface Props {
+  title: string | number;
+  color?: string;
+}
+function Header(props: Props) {
+  return (
+      <header style={{ color: props.color || "red" }}>{props.title}</header>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+            <Header title="Meine Webseite" />
+            <Header title="foo" color="blue" />
+        </div>
   );
 }
 
